2f5dc6db68c12c20f94c0bcca08908e69c5e995d
Fábio Varela <fabio-varela@protonmail.com>
2022-11-28 23:23:03 -0300

    Added database, templates

--- app.py
+++ app.py
@@ -1,13 +1,83 @@
 from flask import (Flask, flash, redirect, render_template, request, session,
                    url_for)
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
-app.secret_key = "Supersecretkey"
+app.secret_key = "supersecretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
+# User Model creation
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique = True)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    def __repr__(self) -> str:
+        return '<Name %r>' % self.name
+#
 
+# User Form
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+#
 
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+#
 
+#Home page
+@app.route('/')
+def index():
+    return render_template("index.html")
+#
 
+#Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted Succesfully!")
+    return render_template('name.html',name = name, form = form)
+#
+
+#User addapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email = form.email.data).first()
+        if user is None:
+            user = Users(name = form.name.data, email = form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User created!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html",form = form, name = name, our_users = our_users)
+#
+
+#Error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+#
 
 
 if __name__ == "__main__":
---
+++ static/app.js
@@ -0,0 +1,5 @@
+setTimeout(
+    () =>{
+        document.getElementById("msg").remove()
+    }
+,2000)
